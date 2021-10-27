ba6d3bc328717b231cb1310f2ff504e65874e1ea
Poltergeist007 <77159690+girish-07@users.noreply.github.com>
2021-10-27 21:25:59 +0530

    Sorting an array of 0, 1 and 2
    Sorting an array of 0, 1 and 2 without using sorting algorithm

---
+++ 4_Sort_array_of_0_1_2.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, low=0, mid=0, high;
+    cout << "\nEnter the number of inputs: ";
+    cin >> n;
+    high = n - 1;
+    int arr[n];
+    cout << "\nEnter the array: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    while(mid<=high) {
+        if(arr[mid]==0) {
+            swap(arr[mid++], arr[low++]);
+        }
+        else if(arr[mid]==1) {
+            mid++;
+        }
+        else {
+            swap(arr[mid], arr[high--]);
+        }
+    }
+    cout << "\nSorted array is: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+}
