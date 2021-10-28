fed6068728050146fb01708fafd03e8f357515a0
Poltergeist007 <77159690+girish-07@users.noreply.github.com>
2021-10-28 20:33:36 +0530

    Cyclically Rotate an Array by one
    Cyclically rotate an array by one

---
+++ 7_Cyclically_Rotate_Array_By_One.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "\nEnter the number of elements in the array: ";
+    cin >> n;
+    int arr[n];
+    cout << "\nEnter the array: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    int temp = arr[n - 1];
+    for (int i = (n - 1); i >= 1; i--) {
+        arr[i] = arr[i - 1];
+    }
+    arr[0] = temp;
+    cout << "\nArray cyclically rotated by one is ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
